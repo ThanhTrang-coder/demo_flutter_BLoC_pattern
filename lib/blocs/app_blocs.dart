9b19142ac8a2604d6488a0ec6147507a38fc50ad
ThanhTrang-coder <lethanhtrang12345@gmail.com>
2023-08-19 08:35:35 +0700

    complete flutter BLoC pattern

---
+++ lib/blocs/app_blocs.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_pattern/blocs/app_events.dart';
+import 'package:flutter_bloc_pattern/blocs/app_state.dart';
+import 'package:flutter_bloc_pattern/repository/repository.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final UserRepository _userRepository;
+
+  UserBloc(this._userRepository): super(UserLoadingState()) {
+    //UI trigger event, then call UserLoadingState()
+    //at the same time, we load data from server, then load success,
+    //we call UserLoadedState()
+    //if there have the error, we call UserErrorState()
+    on<LoadUserEvent>((event, emit) async {
+      emit(UserLoadingState());
+      try {
+        final users = await _userRepository.getUsers();
+        emit(UserLoadedState(users));
+      } catch (e) {
+        emit(UserErrorState(e.toString()));
+      }
+    });
+  }
+}
---
+++ lib/blocs/app_state.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc_pattern/models/user_model.dart';
+
+@immutable
+abstract class UserState extends Equatable {}
+
+//data loading state
+class UserLoadingState extends UserState {
+  @override
+  List<Object?> get props => [];
+}
+
+//data loaded state
+class UserLoadedState extends UserState {
+  final List<UserModel>  users;
+
+  UserLoadedState(this.users);
+
+  @override
+  List<Object?> get props => [users];
+}
+
+//data error loading state
+class UserErrorState extends UserState {
+  final String error;
+
+  UserErrorState(this.error);
+  @override
+  List<Object?> get props => [error];
+}
